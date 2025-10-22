52657b67ccee9930d458bd78a26d195e05ea201b
Edric Tran <6289632@edu.vaniercollege.qc.ca>
2025-10-22 11:08:29 -0400

    Full implementation of part 1

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -4,17 +4,60 @@
  */
 package javaapplication1;
 
+import java.time.Duration;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.animation.TranslateTransition;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
 /**
  *
  * @author 6289632
  */
-public class JavaApplication1 {
+public class JavaApplication1 extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
-    
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane bp = new BorderPane();
+
+
+        Circle circle = new Circle();
+        circle.setRadius(50);
+        circle.setLayoutX(60);
+        circle.setLayoutY(60);
+        circle.setFill(Color.RED);
+        
+        Rectangle rectangle = new Rectangle(400, 150);
+        PathTransition pt = new PathTransition();
+        pt.setPath(rectangle);
+        pt.setNode(circle);
+        pt.setCycleCount(Timeline.INDEFINITE);
+        pt.setAutoReverse(false);
+        pt.play();
+        
+        Line line = new Line(0, 300, 800, 300);
+
+        Pane pane = new Pane(circle, line);
+        bp.setTop(pane);
+
+        Scene scene = new Scene(bp, 800, 600);
+        stage.setScene(scene);
+        stage.setTitle("Lab 07");
+        stage.show();
+    }
 }
